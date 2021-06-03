fd55e4b3efec99af79322da4d9702842f3d701a0
Lalenadr <81616554+Lalenadr@users.noreply.github.com>
2021-06-03 14:56:23 +0200

    Works

--- count.js
+++ count.js
@@ -1,24 +1,39 @@
-let currentDate = new Date ();
-let RandomDate = new Date ('2021-06-01');
-let count = RandomDate - currentDate;
+//let currentDate = new Date ().getTime(); // ik weet niet goed wat die getTime doet, geeft een nummer in tegenstelling tot de datum
+//let RandomDate = new Date ('2022-06-01').getTime(); // hier ook dus
+// let count = RandomDate - currentDate; // resultaat is een nummer
 
-var days = Math.floor(count / (1000 * 60 * 60 * 24));
+// hieronder standaard berekeningen - die zijn dus ok - die gooien we ook even in de functie
+
+/*var days = Math.floor(count / (1000 * 60 * 60 * 24));
 var hours = Math.floor((count % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 var minutes = Math.floor((count % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((count % (1000 * 60)) / 1000);
 
-console.log(currentDate);
-console.log(RandomDate);
-console.log(count);
-console.log(days,hours,minutes,seconds);
+console.log(currentDate); // nummer
+console.log(RandomDate); // nummer
+console.log(count); // nummer
+console.log(days,hours,minutes,seconds); // klopt*/
 
-let i=0;
+/*var x =*/ setInterval(function() {
+    let currentDate = new Date ().getTime();
+    let RandomDate = new Date ('2022-06-01').getTime();
+    let count = RandomDate - currentDate;
+      
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(count / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((count % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((count % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((count % (1000 * 60)) / 1000);
 
-//setInterval (function() {i++, console.log(minutes)}),10000; LAPTOP GOES LOCO WITH THIS
-
-var myfunc = setInterval(function() {i++
-
-document.getElementById("days").innerHTML = days + "d "
-document.getElementById("hours").innerHTML = hours + "h " 
-document.getElementById("mins").innerHTML = minutes + "m " 
-document.getElementById("secs").innerHTML = seconds + "s" },5000);
+    console.log(days,hours,minutes,seconds) // CTRL + C om da spel stop te zetten
+      
+    // Output the result in an element with id="demo"
+    /*document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+      
+    // If the count down is over, write some text 
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("demo").innerHTML = "EXPIRED";
+    }*/
+  }, 1000);
